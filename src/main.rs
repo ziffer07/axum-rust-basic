f6e2111573d0f97ec4e98949d49f146345c7f56f
Aryan <ksharyan18@gmail.com>
2026-04-20 16:36:01 +0530

    Added some post functionality and redirects. Now the app is more dynamic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use askama::{Template};
 use axum::{
-    Router, extract::Query, http::StatusCode, response::{Html, IntoResponse, Response}, routing::get
+    Form, Router, extract::Query, http::StatusCode, response::{Html, IntoResponse, Redirect, Response}, routing::{get, post}
 };
 use serde::{Serialize, Deserialize};
 
@@ -32,12 +32,23 @@ struct EchoTemplate {
     text: String,
 }
 
+#[derive(Template)]
+#[template(path = "../templates/post_form.html")]
+struct PostTemplate {}
+
+#[derive(Template)]
+#[template(path = "../templates/echo_post.html")]
+struct EchoPostTemplate {
+    text_in: String,
+}
+
 #[derive(Serialize, Deserialize)]
 #[allow(dead_code)]
 struct Params {
     text: String,
 }
 
+// Get instruction
 async fn main_page() -> impl IntoResponse {
     let template = MainTemplate{};
     HtmlTemplate(template)
@@ -50,10 +61,38 @@ async fn echo(Query(input_text): Query<Params>) -> impl IntoResponse {
     HtmlTemplate(template)
 }
 
+
+// Redirects
+async fn go_to_post() -> Redirect {
+    Redirect::temporary("/post-form")
+}
+
+async fn home_redirect() -> Redirect {
+    Redirect::temporary("/")
+}
+
+//Post Instructions
+async fn post_form() -> impl IntoResponse {
+    let template = PostTemplate{};
+    HtmlTemplate(template)
+}
+
+async fn echo_post(Form(input): Form<Params>) -> impl IntoResponse {
+    let template =  EchoPostTemplate {
+        text_in: input.text,
+    };
+    HtmlTemplate(template)
+}
+
 fn create_router() -> Router {
     Router::new()
         .route("/", get(main_page))
         .route("/echo", get(echo))
+        .route("/home_redirect", get(home_redirect))
+        .route("/post-form", get(post_form))
+        .route("/echo-post", post(echo_post))
+        .route("/post-form-redirect", get(go_to_post))
+        
 
 }
 
@@ -61,7 +100,7 @@ fn create_router() -> Router {
 async fn main() {
     let app = create_router();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await.unwrap();
     println!("Listenening on http://{}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
